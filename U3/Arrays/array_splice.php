903a3f9aef83e293bc5950134dd350b56397b7c7
Toni <tamengual9@gmail.com>
2018-03-31 15:12:11 +0200

    afegit material pendent organitzar

---
+++ U3/Arrays/array_splice.php
@@ -0,0 +1,18 @@
+
+<h1>Eliminar un boci d'un array</h1>
+<h4>Abans de retallar:<h4>
+<?php
+$lletres=range('a','z');
+var_dump($lletres);
+?>
+
+<h4>Després de retallar:<h4>
+<?php
+$retallat = array_splice($lletres,7,9);
+var_dump($lletres);
+ ?>
+
+ <h1>Substituir un bocí d'un array per un altre</h1>
+ <?php
+
+  ?>
